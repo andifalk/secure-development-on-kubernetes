fa2f48172a2c185ef72607d515c371292727cd70
Andreas Falk <andreas.falk@novatec-gmbh.de>
2020-02-11 22:56:48 +0100

    update gke version in script

--- gke-provisioning/README.md
+++ gke-provisioning/README.md
@@ -1,5 +1,4 @@
 # Demo K8s cluster on Google Cloud
 
-Her you find helpful scripts to setup & access 
+Her you find helpful scripts to setup & access
 GKE on Google Cloud.
-
--- gke-provisioning/create-gke.sh
+++ gke-provisioning/create-gke.sh
@@ -2,7 +2,7 @@
 
 # create kubernetes cluster using encryption at rest and application level encryption
 gcloud beta container --project "pa-afa-kubernetes" clusters create "demo-gke" --zone "europe-west3-a" \
---no-enable-basic-auth --cluster-version "1.14.8-gke.12" --machine-type "n1-standard-2" --image-type "COS" \
+--no-enable-basic-auth --cluster-version "1.15.8-gke.3" --machine-type "n1-standard-2" --image-type "COS" \
 --disk-type "pd-standard" --disk-size "100" --metadata disable-legacy-endpoints=true \
 --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write", \
 "https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol", \
